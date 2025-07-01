063696fa80a415423a30ea4ad6a804b1273ce74b
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-01 23:25:24 +0200

    teaImgs [] and imgs added

---
+++ script.js
@@ -0,0 +1,14 @@
+const teaImgs = [
+  "./img/t1.png",
+  "./img/t2.png",
+  "./img/t3.png",
+  "./img/t4.png",
+  "./img/t5.png",
+  "./img/t6.png",
+  "./img/t7.png",
+  "./img/t8.png",
+  "./img/t9.png",
+  "./img/t10.png",
+  "./img/t11.png",
+  "./img/t12.png",
+];
