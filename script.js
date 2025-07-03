b7e93468ea38555f67ec27bb63a69439ea989b35
Sascha Heinze <sascha.heinze.dev@gmail.com>
2025-07-03 09:02:21 +0200

    comments added

--- script.js
+++ script.js
@@ -14,25 +14,31 @@ const teaArray = [
 ];
 
 // Referenzen auf Overlay-Elemente
+
 // "document.getElementById" oder "document.getElementByClass"
 // "QuerySelector" sucht ein css-Element, dass zur entsprechenden Klasse passt
-const overlay = document.getElementById("overlay"); // ID="overlay" wird zu einer Konstanten, "overlay" heißt"
+const overlay = document.getElementById("overlay"); // ID="overlay" wird zu einer Konstanten, die "overlay" heißt"
 const overlayImage = document.getElementById("overlayImage"); // ID="overlayImage" wird zu einer Konstanten, die "overlayImage" heißt
 const closeButton = document.querySelector(".closeOverlay"); // class="closeOverlay" wird zu einer Konstanten, die "closeButton" heißt
 const imageContainer = document.getElementById("imageContainer"); // ID="imageContainer" wird zu einer Konstanten, die "imageContainer" heißt
 
 // Schließen-Handler
+
+// "click" auf den close.Button ruft das "overlay" auf (in dem Fall wird aufgerufen, dass etwas verschwindet)
 closeButton.addEventListener("click", () => {
-  overlay.classList.add("d_none");
+  // "click" ist ein Event (wie "keydown") und ruft auf:
+  overlay.classList.add("d_none"); // ... dass die Konstante overlay "d_none" anzeigen soll
 });
 
 // Vorschaubilder erzeugen und in den Container hängen
+
+// jedes Bild wird "angeschaut"
 teaArray.forEach((imageUrl) => {
-  const teaCup = document.createElement("img");
-  teaCup.src = imageUrl;
-  teaCup.alt = "a nice cup of tea";
-  teaCup.addEventListener("click", functionImageClick);
-  imageContainer.appendChild(teaCup);
+  const teaCup = document.createElement("img"); // für jedes Bild (im Array), das angeschaut wurde, wird ein leerer Platzhalter erstellt der "teaCup" heißt
+  teaCup.src = imageUrl; // Bild wird auf Platz "teaCup" gelegt
+  teaCup.alt = "a nice cup of tea"; // Alternativtext
+  teaCup.addEventListener("click", functionImageClick); // beim "click" wird die "functionImageClick" aufgerufen und ausgeführt
+  imageContainer.appendChild(teaCup); // setzt das neue Bild in den von uns definierten Bereich (ImageContainer)
 });
 
 // Funktion, die für jedes Vorschaubild aufgerufen wird
